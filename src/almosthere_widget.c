ea62c6b7d809afb6e411958a81ae4cb18d80a0ef
Danilo Horta <danilo.horta@gmail.com>
2018-01-04 18:07:06 +0000

    curses

--- src/almosthere_widget.c
+++ src/almosthere_widget.c
@@ -1,4 +1,5 @@
 #include "almosthere_widget.h"
+#include "terminal.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,7 +7,7 @@ void almosthere_widget_create(struct almosthere_widget **widget) {
 
     *widget = malloc(sizeof(struct almosthere_widget));
     (*widget)->consumed = -1.0;
-    (*widget)->length = 70;
+    (*widget)->length = almosthere_get_term_width();
 }
 
 void almosthere_widget_finish(struct almosthere_widget *widget) {
@@ -35,26 +36,4 @@ void almosthere_widget_update(struct almosthere_widget *widget, double consumed,
         widget->consumed = consumed;
 
     almosthere_widget_draw(widget);
-    // long i;
-    //
-    // double coef = 1.0 / (1000 * 1000 * 1000);
-    // double dlt = coef * dlt_nsec;
-    //
-    // long ndots = (long)(at->speed * dlt);
-    // double ndots_remainder = at->speed * dlt - ((double)ndots);
-    // double rest = ndots_remainder + at->drawn_remainder;
-    // ndots += (long)rest;
-    // rest -= (double)((long)rest);
-    //
-    // if (at->drawn + ndots > at->consumed)
-    //   ndots = at->consumed - at->drawn;
-    //
-    // for (i = 0; i < ndots; ++i) {
-    //   putchar('.');
-    // }
-    // fflush(stdout);
-    //
-    // at->drawn += ndots;
-    // at->drawn_remainder = rest;
-    // // printf("%.60f\n", line->drawn_remainder);
 }
--- src/terminal.h
+++ src/terminal.h
@@ -1,55 +1,6 @@
 #ifndef ALMOSTHERE_TERMINAL_H_
 #define ALMOSTHERE_TERMINAL_H_
 
-unsigned get_term_width(void) {
-  int cols = 0;
-  int tty_fd = -1;
-
-  char const *const term = getenv("TERM");
-  if (!term) {
-    fprintf(stderr, "TERM environment variable not set\n");
-    return 0;
-  }
-
-  char const *const cterm_path = ctermid(NULL);
-  if (!cterm_path || !cterm_path[0]) {
-    fprintf(stderr, "ctermid() failed\n");
-    return 0;
-  }
-
-  tty_fd = open(cterm_path, O_RDWR);
-  if (tty_fd == -1) {
-    fprintf(stderr, "open(\"%s\") failed (%d): %s\n", cterm_path, errno,
-            strerror(errno));
-    return 0;
-  }
-
-  int setupterm_err;
-  if (setupterm((char *)term, tty_fd, &setupterm_err) == ERR) {
-    switch (setupterm_err) {
-    case -1:
-      fprintf(stderr, "setupterm() failed: terminfo database not found\n");
-      goto done;
-    case 0:
-      fprintf(
-          stderr,
-          "setupterm() failed: TERM=%s not found in database or too generic\n",
-          term);
-      goto done;
-    case 1:
-      fprintf(stderr, "setupterm() failed: terminal is hardcopy\n");
-      goto done;
-    } // err
-  }
-
-  cols = tigetnum((char *)"cols");
-  if (cols < 0)
-    fprintf(stderr, "tigetnum() failed (%d)\n", cols);
-
-done:
-  if (tty_fd != -1)
-    close(tty_fd);
-  return cols < 0 ? 0 : cols;
-}
+unsigned almosthere_get_term_width(void);
 
 #endif /* end of include guard: ALMOSTHERE_TERMINAL_H_ */
