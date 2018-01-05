01076a006422db078d438030d7a0ce5a050d3fcb
Danilo Horta <danilo.horta@gmail.com>
2018-01-05 13:09:17 +0000

    working resize canvas

--- src/almosthere.c
+++ src/almosthere.c
@@ -13,67 +13,24 @@ static const double MINIMUM_DELTA = 0.01;
 static const double TIMESTEP = 1.0 / 30.0;
 
 struct almosthere {
-    long volume;
-    long consumed;
-    double speed;
-    struct timespec *last_update;
-    struct timespec delta_start;
-    long consumed_start;
+    long volume;                  /* total size to be consumed */
+    long consumed;                /* how much have been consumed */
+    double speed;                 /* current consumption speed */
+    struct timespec *last_update; /* last timestamp of update call */
+    struct timespec delta_start;  /* last timestamp used as delta start */
+    long consumed_start; /* how much was already consumed at delta_start */
 
-    thrd_t thr;
-    int stop_thread;
+    thrd_t thr;      /* spawed thread to perfom update and draw */
+    int stop_thread; /* send stop thread signal */
 
-    struct widget *line;
+    struct widget *line; /* root widget */
 };
 
-void almosthere_update_speed(struct almosthere *at);
-
-void almosthere_update(struct almosthere *at) {
-
-    struct timespec curr, diff;
-    double dlt, consumed;
-
-    almosthere_update_speed(at);
-
-    almosthere_timespec_get(&curr);
-
-    if (at->last_update == NULL) {
-        // This is the first update call.
-        at->last_update = malloc(sizeof(struct timespec));
-        *at->last_update = curr;
-    }
-
-    almosthere_timespec_diff(at->last_update, &curr, &diff);
-    dlt = almosthere_timespec_sec(&diff);
-
-    consumed = ((double)at->consumed) / at->volume;
-
-    at->line->update(at->line, consumed, at->speed, dlt);
-
-    *at->last_update = curr;
-}
-
-int almosthere_thread_start(void *args) {
-
-    struct almosthere *at = (struct almosthere *)args;
-
-    almosthere_update(at);
-    while (at->stop_thread == 0) {
-        almosthere_thread_sleep(TIMESTEP);
-        almosthere_update(at);
-    }
-
-    return 0;
-}
-
-void almosthere_create_line(struct widget **line) {
-
-    struct widget **widget = malloc(1 * sizeof(struct widget *));
-
-    widget[0] = widget_bar_create();
-    *line = widget_line_create(1, widget);
-    free(widget);
-}
+void update_speed(struct almosthere *at);
+int thread_start(void *args);
+void create_line(struct widget **line);
+void update_speed(struct almosthere *at);
+void update(struct almosthere *at);
 
 struct almosthere *almosthere_create(long volume) {
 
@@ -87,19 +44,60 @@ struct almosthere *almosthere_create(long volume) {
     almosthere_timespec_get(&at->delta_start);
     at->consumed_start = 0;
 
-    almosthere_create_line(&at->line);
+    create_line(&at->line);
 
     at->stop_thread = 0;
-    status = thrd_create(&at->thr, almosthere_thread_start, at);
+    status = thrd_create(&at->thr, thread_start, at);
     if (status != thrd_success) {
-        fprintf(stderr, "Could not spawn a thread.");
+        fprintf(stderr, "Could not spawn a thread.\n");
         return NULL;
     }
 
     return at;
 }
 
-void almosthere_update_speed(struct almosthere *at) {
+void almosthere_consume(struct almosthere *at, long consume) {
+    if (at->consumed + consume > at->volume) {
+        fprintf(stderr, "User trying to consume more than the total volume.\n");
+        at->consumed = at->volume;
+    } else
+        at->consumed += consume;
+}
+
+void almosthere_finish(struct almosthere *at) {
+
+    at->stop_thread = 1;
+    thrd_join(at->thr, NULL);
+    if (at->last_update != NULL)
+        free(at->last_update);
+
+    at->line->finish(at->line);
+    free(at);
+}
+
+int thread_start(void *args) {
+
+    struct almosthere *at = (struct almosthere *)args;
+
+    update(at);
+    while (at->stop_thread == 0) {
+        almosthere_thread_sleep(TIMESTEP);
+        update(at);
+    }
+
+    return 0;
+}
+
+void create_line(struct widget **line) {
+
+    struct widget **widget = malloc(1 * sizeof(struct widget *));
+
+    widget[0] = widget_bar_create();
+    *line = widget_line_create(1, widget);
+    free(widget);
+}
+
+void update_speed(struct almosthere *at) {
     struct timespec curr, diff;
     long consumed;
     double dlt;
@@ -117,20 +115,27 @@ void almosthere_update_speed(struct almosthere *at) {
     }
 }
 
-void almosthere_consume(struct almosthere *at, long consume) {
-    if (at->consumed + consume > at->volume)
-        at->consumed = at->volume;
-    else
-        at->consumed += consume;
-}
+void update(struct almosthere *at) {
 
-void almosthere_finish(struct almosthere *at) {
+    struct timespec curr, diff;
+    double dlt, consumed;
 
-    at->stop_thread = 1;
-    thrd_join(at->thr, NULL);
-    if (at->last_update != NULL)
-        free(at->last_update);
+    update_speed(at);
 
-    at->line->finish(at->line);
-    free(at);
+    almosthere_timespec_get(&curr);
+
+    if (at->last_update == NULL) {
+        // This is the first update call.
+        at->last_update = malloc(sizeof(struct timespec));
+        *at->last_update = curr;
+    }
+
+    almosthere_timespec_diff(at->last_update, &curr, &diff);
+    dlt = almosthere_timespec_sec(&diff);
+
+    consumed = ((double)at->consumed) / at->volume;
+
+    at->line->update(at->line, consumed, at->speed, dlt);
+
+    *at->last_update = curr;
 }
