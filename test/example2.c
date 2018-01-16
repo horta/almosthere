c00843cf9ae690b181ed51fa4fff5bd0fae6dfa4
Danilo Horta <danilo.horta@gmail.com>
2018-01-16 10:16:45 +0000

    Examples

---
+++ test/example2.c
@@ -0,0 +1,15 @@
+#include "athr.h"
+
+int main() {
+    struct athr *at = athr_create(100, "My tasks");
+    int i;
+
+    for (i = 0; i < 100; ++i) {
+        athr_sleep(50); /* some time-consuming task */
+        athr_consume(at, 1);
+    }
+
+    athr_finish(at);
+
+    return 0;
+}
