299396f05de132304f7747f3f466a518ba8ab8aa
Danilo Horta <danilo.horta@pm.me>
2019-11-13 12:23:43 +0000

    attr format'

--- src/report.c
+++ src/report.c
@@ -1,4 +1,5 @@
 #include "report.h"
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 
--- src/report.h
+++ src/report.h
@@ -1,8 +1,12 @@
 #ifndef ATHR_REPORT_H
 #define ATHR_REPORT_H
 
-#include <stdarg.h>
+#if defined(HAVE_ATTR_FORMAT)
+#define ATTR_FORMAT __attribute__((format(printf, 1, 2)))
+#else
+#define ATTR_FORMAT
+#endif
 
-void athr_fatal(char const* err, ...) __attribute__((format(printf, 1, 2)));
+void athr_fatal(char const* err, ...) ATTR_FORMAT;
 
 #endif
