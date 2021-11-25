de9ecded8bd04f0b79490714333e400a991eaaee
Danilo Horta <danilo.horta@pm.me>
2021-11-25 23:52:09 +0000

    check snprintf ret to please gcc

--- src/widget_eta.c
+++ src/widget_eta.c
@@ -19,6 +19,8 @@ static inline unsigned day_sec(double s) { return (unsigned)(s / DAY_SEC); }
 static inline unsigned hour_sec(double s) { return (unsigned)(s / HOUR_SEC); }
 static inline unsigned min_sec(double s) { return (unsigned)(s / MIN_SEC); }
 
+#define xsnprintf(...) snprintf(__VA_ARGS__) < 0 ? abort() : (void)0
+
 static void update(struct athr_widget *w, double consumed, double speed)
 {
     unsigned const sz = ATHR_WIDGET_ETA_LEN;
@@ -26,26 +28,26 @@ static void update(struct athr_widget *w, double consumed, double speed)
     eta->consumed = consumed;
 
     if (speed < MIN_SPEED)
-        snprintf(eta->buff, sz + 1, "%*s", sz, " - eta - ");
+        xsnprintf(eta->buff, sz + 1, "%*s", sz, " - eta - ");
     else
     {
         int secs = (int)((1 - eta->consumed) / speed);
 
         /* Based on wget progress module */
         if (secs < 100)
-            snprintf(eta->buff, sz + 1, "%*ds", sz - 1, secs);
+            xsnprintf(eta->buff, sz + 1, "%*ds", sz - 1, secs);
         else if (secs < 100 * 60)
-            snprintf(eta->buff, sz + 1, "%*dm %*ds", sz - 3 - 2, secs / 60, 2,
-                     secs % 60);
+            xsnprintf(eta->buff, sz + 1, "%*dm %*ds", sz - 3 - 2, secs / 60, 2,
+                      secs % 60);
         else if (secs < 48 * 3600)
-            snprintf(eta->buff, sz + 1, "%*dh %*dm", sz - 3 - 2, secs / 3600, 2,
-                     (secs / 60) % 60);
+            xsnprintf(eta->buff, sz + 1, "%*dh %*dm", sz - 3 - 2, secs / 3600,
+                      2, (secs / 60) % 60);
         else if (secs < 100 * 86400)
-            snprintf(eta->buff, sz + 1, "%*dd %*dh", sz - 3 - 2, secs / 86400,
-                     2, (secs / 3600) % 24);
+            xsnprintf(eta->buff, sz + 1, "%*dd %*dh", sz - 3 - 2, secs / 86400,
+                      2, (secs / 3600) % 24);
         else
             /* even (2^31-1)/86400 doesn't overflow eta->buff. */
-            snprintf(eta->buff, sz + 1, "%*dd", sz - 1, secs / 86400);
+            xsnprintf(eta->buff, sz + 1, "%*dd", sz - 1, secs / 86400);
     }
 
     for (unsigned i = 0; i < sz; ++i)
